3f04a0f957d2569170565cb7c877d7d3eed9ca51
Vadym <tvadost2003@gmail.com>
2021-02-05 14:09:49 +0200

    fix

--- app.js
+++ app.js
@@ -6,22 +6,29 @@ const BotEvents = require('viber-bot').Events;
 const app = express();
 
 const bot = new ViberBot({
-  authToken: "4cc699063f67df1b-b28334184d4dc200-adfff3aa8ea89b2c",
-  name: "EchoBot",
-  avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
+    authToken: "4cc699063f67df1b-b28334184d4dc200-adfff3aa8ea89b2c",
+    name: "Cat Vet Bot",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
 });
 
+
+bot.on(BotEvents.SUBSCRIBED, response => {
+    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
+});
 
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-  response.send(message + " МОЙ БОТ");
+    response.send(new TextMessage(`Message received.`));
 });
 
-app.use("https://viber-kmrf-bot.herokuapp.com", bot.middleware());
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 8080;
+app.use("/viber/webhook", bot.middleware());
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-module.exports = app;
+    console.log(`Application running on port: ${port}`);
+    bot.setWebhook(`https://viber-kmrf-bot.herokuapp.com`).catch(error => {
+        console.log('Can not set webhook on following server. Is it running?');
+        console.error(error);
+        process.exit(1);
+    });
+});
