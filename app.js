5f2345a276346530e2c69166f1dbb3a2ee0fb2a6
Vadym <tvadost2003@gmail.com>
2021-02-05 14:38:06 +0200

    kb

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 
 const TextMessage = require('viber-bot').Message.Text;
+const KeyboardMessage = require('viber-bot').Message.Keyboard;
 
 const app = express();
 
@@ -13,16 +14,37 @@ const bot = new ViberBot({
     avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
 });
 
-
 bot.on(BotEvents.SUBSCRIBED, response => {
     response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
 });
+
+const SAMPLE_KEYBOARD = {
+    "Type": "keyboard",
+    "Revision": 1,
+    "Buttons": [
+        {
+            "Columns": 3,
+            "Rows": 2,
+            "BgColor": "#e6f5ff",
+            "BgMedia": "http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg",
+            "BgMediaType": "picture",
+            "BgLoop": true,
+            "ActionType": "reply",
+            "ActionBody": "Yes"
+        }
+    ]
+};
+
+const message = new KeyboardMessage(SAMPLE_KEYBOARD);
 
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
     console.log("MESSAGE: ", message)
     switch (message.text.toLowerCase()) {
         case "ты чей?":
             response.send(new TextMessage("Мой конечно же"));
+            break;
+        case "cmd":
+            response.send(message)
             break;
         default:
             response.send(new TextMessage(message.text));
